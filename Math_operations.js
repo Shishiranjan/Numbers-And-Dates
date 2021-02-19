2c64bb5b42f22e5179fe250ed07a978f31fe7710
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-19 16:06:32 +0545

    Add files via upload
    Here we basically see all the basic mathematical operations like addition, subtraction, multiplication, division, finding the square root of a number etc.

---
+++ Math_operations.js
@@ -0,0 +1,25 @@
+//You can easily be able to do all of the mathematical operations in JS like addition, subtractio,
+//multiplication, division etc.
+//For finding the square root of any number simply, use the sqrt method of math.
+console.log(math.sqrt(81));
+//Alternative way for finding the square root of any  number.
+
+console.log(64 ** (1 / 2));
+// But to find the cubic root of any method there is no any method in javascript. But the alternative of
+//doing it is given below.
+
+console.log(1000 ** (1 / 3));
+
+//For finding the maximum value from a list of numbers.
+console.log(Math.max(14, 15, 16, 25, 71, 11));
+
+//For finding the minimum element from a list of numbers.
+
+console.log(Math.min(78, 14, 75, 52, 67, 19));
+
+//To find the value of pi just use PImethod.
+console.log(Math.PI);
+
+//Finding the area of circle
+
+console.log(Math.PI * Number.parseFloat("10px") ** 2);
