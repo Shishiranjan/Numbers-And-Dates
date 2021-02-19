fea4f66667b9d9a85916728fe05db45940544d5c
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-19 16:07:50 +0545

    Add files via upload

---
+++ remainder_operator.js
@@ -0,0 +1,9 @@
+//this operator just returns the remainder of any divisor.
+
+console.log(5 % 2);
+console.log(8 % 2);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(64));
+console.log(isEven(747));
